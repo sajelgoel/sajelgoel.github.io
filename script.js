9a503291402f129a3c2a68cfa76d2c7f38480165
Sajel <sajel_goel@yahoo.com>
2020-10-02 14:43:13 +0530

    active class added to navigation

--- script.js
+++ script.js
@@ -3,14 +3,23 @@ function changeView(page){
         document.getElementById('profile').style.display='block'
         document.getElementById('project').style.display='none'
         document.getElementById('contact').style.display='none'
+        document.getElementById('list-profile').classList.add('active');
+        document.getElementById('list-project').classList.remove('active');
+        document.getElementById('list-contact').classList.remove('active');
     }else if(page=='project'){
         document.getElementById('profile').style.display='none'
         document.getElementById('project').style.display='block'
         document.getElementById('contact').style.display='none'
+        document.getElementById('list-profile').classList.remove('active');
+        document.getElementById('list-project').classList.add('active');
+        document.getElementById('list-contact').classList.remove('active');
     }else if(page=='contact'){
         document.getElementById('profile').style.display='none'
         document.getElementById('project').style.display='none'
         document.getElementById('contact').style.display='block'
+        document.getElementById('list-profile').classList.remove('active');
+        document.getElementById('list-project').classList.remove('active');
+        document.getElementById('list-contact').classList.add('active');
     }
 
 }
